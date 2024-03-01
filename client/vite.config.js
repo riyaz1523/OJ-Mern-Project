bf92e9f8ac1001d1a2bfe9b25750b7293834f434
Riyaz <riyazramzan2001@gmail.com>
2024-03-01 22:37:48 +0530

    made changes in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -19,6 +19,11 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
   plugins: [react()],
   define: { global: 'globalThis'} 
 });
